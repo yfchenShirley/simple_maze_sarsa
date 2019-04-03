a55b641c2c6ca580ae37b436b1cb073a066aaf34
yfchenShirley <641751045@qq.com>
2019-04-03 14:34:55 +0200

    plot total rewards at each episode

--- run_this.py
+++ run_this.py
@@ -2,9 +2,9 @@
 Reinforcement learning maze example.
 
 Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
+Black rectangles:       walls       [reward = -5].
+Yellow bin circle:      exit    [reward = +100].
+All other states:       ground      [reward = -1].
 
 This script is the main part which controls the update method of this example.
 The RL is in RL_brain.py.
@@ -15,9 +15,12 @@ View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 from maze_env import Maze
 from Sarsa_brain import SarsaTable
 
+EPIS = 1000
+plot_x = []
 
 def update():
-    for episode in range(1000):
+    for episode in range(EPIS):
+        
         # initial observation
         observation = env.reset()
         reward_total = 0
@@ -44,11 +47,30 @@ def update():
 
             # break while loop when end of this episode
             if done:
+                plot_x.append(reward_total)
                 print(f"Episode {episode}:")
                 print(f"Total rewards: {reward_total}")
                 break
 
     # end of game
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Data for plotting
+   
+    s = 1 + np.sin(2 * np.pi * t)
+
+    fig, ax = plt.subplots()
+    ax.plot(plot_x, range(EPIS))
+
+    ax.set(xlabel='episode (s)', ylabel='Total Rewards',
+           title='Total rewards at each episode')
+    ax.grid()
+
+    fig.savefig("test.png")
+    plt.show()
+
     print('game over')
     env.destroy()
 
